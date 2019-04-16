d17b165695c8f1a461ac18f5e73d7f627c4a4f3d
贾新 <1623416514@qq.com>
2019-04-16 19:41:06 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-class Person {
-    constructor(name, age) {
+//多态
+class People {
+    constructor(name, house) {
         this.name = name
-        this.age = age
+        this.house = house
     }
-    eat() {
-        alert(`${this.name} eat something`)
-    }
-    speak() {
-        alert(`${this.name} age is ${this.age}`)
+    saySomething() {
+
     }
 }
-let zhang = new Person('Zhang', 30)
-zhang.eat()
-zhang.speak()
-let jia = new Person('Jia', 30)
+class A extends People {
+    constructor(name, house) {
+        super(name, house)
+    }
+    saySomething() {
+        alert('I am A')
+    }
+}
+class B extends People {
+    constructor(name, house) {
+        super(name, house)
+    }
+    saySomething() {
+        alert('I am B')
+    }
+}
+class House {
+    constructor(city) {
+        this.city = city
+    }
+    showCity() {
+        alert(`house in ${this.city}`)
+    }
+}
+let aHouse = new House('beijing')
+let a = new A('a', aHouse)
+a.saySomething()
+console.log(a)
+let b = new B('b')
+b.saySomething()
